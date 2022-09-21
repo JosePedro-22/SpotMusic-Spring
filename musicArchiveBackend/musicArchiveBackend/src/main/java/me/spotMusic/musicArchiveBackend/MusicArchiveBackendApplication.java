d328e0f34cb5c1d584e54efba27c6afa6f414290
JosePedro-22 <jp14274@gmail.com>
2022-09-21 17:17:48 -0300

    feat: criando spotMusic backend

---
+++ musicArchiveBackend/musicArchiveBackend/src/main/java/me/spotMusic/musicArchiveBackend/MusicArchiveBackendApplication.java
@@ -0,0 +1,13 @@
+package me.spotMusic.musicArchiveBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicArchiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicArchiveBackendApplication.class, args);
+	}
+
+}
