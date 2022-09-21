29ec0e5e76546fafa50b902672d59a6a23b09a69
JosePedro-22 <jp14274@gmail.com>
2022-09-21 18:25:34 -0300

    feat: refactore project

---
+++ musicArchiveBackend/spotMusicArchive/src/main/java/me/spotMusicArchive/spotMusicArchive/SpotMusicArchiveApplication.java
@@ -0,0 +1,14 @@
+package me.spotMusicArchive.spotMusicArchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotMusicArchiveApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpotMusicArchiveApplication.class, args);
+	}
+
+}
